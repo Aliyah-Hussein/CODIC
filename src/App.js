92b87ddbed9a628072734adac95d495ac1b82a3d
Aliyah-Hussein <90883714+Aliyah-Hussein@users.noreply.github.com>
2022-11-25 10:01:09 +1200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Home from './components/pages/Home';
+import Hangman from './components/HangmanGame/Hangman';
+import Crossword from './components/pages/Crossword';
+import SignUp from './components/pages/SignUp';
+import SignIn from './components/pages/SignIn';
+import Question from './components/HangmanGame/Question';
+import Level from './components/HangmanGame/Level';
+// import Home_H from './components/HangmanGame/Home'
+import About from './components/pages/About';
+ 
+import Instructions from './components/pages/Instructions';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+          <Routes>
+            {/* <Route path="/" exact component = 'Home' /> */}
+            <Route path='/' exact element={<Home/>} />
+            {/* <Route path='./components/HangmanGame/Home_H' exact element={<Home_H/>} /> */}
+            <Route path='/hangman' exact element={<Hangman/>} />
+            <Route path='/crossword' exact element={<Crossword/>} />
+            <Route path='/sign-up' exact element={<SignUp/>} />
+            <Route path='/question' exact element={<Question/>} />
+            <Route path='/level' exact element={<Level/>} />
+            <Route path='/sign-in' exact element={<SignIn/>} />
+            <Route path='/about' exact element={<About/>} />
+            <Route path='/instructions' exact element={<Instructions/>} />
+
+
+          </Routes>
+      </Router>
+    </>
   );
 }
 
